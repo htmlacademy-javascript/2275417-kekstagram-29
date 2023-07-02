883ebcec40ad1ccf8eb2a78e849a89133277645b
KV <lakrise347@gmail.com>
2023-07-02 18:21:07 +0300

    template to elements

---
+++ js/createPictures.js
@@ -0,0 +1,22 @@
+import { createPhotos } from './data.js';
+
+const pictureList = document.querySelector('.pictures');
+const pictureTemplate = document.querySelector('#picture').content;
+
+const Photos = createPhotos();
+const pictureListFragment = document.createDocumentFragment();
+
+Photos.forEach((item) => {
+  const newPicture = pictureTemplate.cloneNode(true);
+  const img = newPicture.querySelector('.picture__img');
+  const likes = newPicture.querySelector('.picture__likes');
+  const comments = newPicture.querySelector('.picture__comments');
+  img.src = item.url;
+  img.alt = item.description;
+  likes.textContent = item.likes;
+  comments.textContent = item.comments.length;
+
+  pictureListFragment.append(newPicture);
+});
+
+pictureList.appendChild(pictureListFragment);
